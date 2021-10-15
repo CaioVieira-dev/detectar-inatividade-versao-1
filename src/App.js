bd0c59556b2339b06085188e5a816b3198f33e8b
Caio Vieira <caiocesarav0208@gmail.com>
2021-10-15 11:43:13 -0300

    feat: :construction: trabalhar no codigo

--- src/App.js
+++ src/App.js
@@ -1,22 +1,90 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable react-hooks/exhaustive-deps */
 
-function App() {
+import './App.css';
+import React, { useState, useEffect } from 'react'
+import { format } from 'date-fns'
+import { useIdleTimer } from 'react-idle-timer'
+
+ function App () {
+  const timeout = 10000
+  const [remaining, setRemaining] = useState(timeout)
+  const [elapsed, setElapsed] = useState(0)
+  const [lastActive, setLastActive] = useState(+new Date())
+  const [lastEvent, setLastEvent] = useState('Events Emitted on Leader')
+  const [leader, setLeader] = useState(true)
+  
+  const handleOnActive = () => setLastEvent('active')
+  const handleOnIdle = () => {
+    setLastEvent('idle')
+alert('inativo')
+}
+
+  const {
+    reset,
+    pause,
+    resume,
+    getRemainingTime,
+    getLastActiveTime,
+    getElapsedTime,
+    isIdle,
+    isLeader
+  } = useIdleTimer({
+    timeout,
+    onActive: handleOnActive,
+    onIdle: handleOnIdle,
+    crossTab: {
+      emitOnAllTabs: false
+    }
+  })
+
+  const handleReset = () => reset()
+  const handlePause = () => pause()
+  const handleResume = () => resume()
+
+  useEffect(() => {
+    setRemaining(getRemainingTime())
+    setLastActive(getLastActiveTime())
+    setElapsed(getElapsedTime())
+
+    setInterval(() => {
+      setRemaining(getRemainingTime())
+      setLastActive(getLastActiveTime())
+      setElapsed(getElapsedTime())
+      setLeader(isLeader())
+    }, 1000)
+    function handleVisibilityChange(){
+      if(document.visibilityState === 'visible'){
+        handleReset()
+        console.log('resetado \nRemaining Time:',getRemainingTime())
+      }else{
+        handlePause()
+        console.log('pausado \nRemaining Time:',getRemainingTime())
+
+      }
+    }
+    document.addEventListener('visibilitychange',handleVisibilityChange,false)
+    return ()=>{
+      document.removeEventListener('visibilitychange',handleVisibilityChange)
+    }
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-header"> 
+         <div>
+        <h1>Timeout: {timeout}ms</h1>
+        <h1>Time Remaining: {remaining}</h1>
+        <h1>Time Elapsed: {elapsed}</h1>
+        <h1>Last Active: {format(lastActive, 'MM-dd-yyyy HH:MM:ss.SSS')}</h1>
+        <h1>Last Event: {lastEvent}</h1>
+        <h1>Is Leader: {leader.toString()}</h1>
+        <h1>Idle: {isIdle().toString()}</h1>
+      </div>
+      <div>
+        <button onClick={handleReset}>RESET</button>
+        <button onClick={handlePause}>PAUSE</button>
+        <button onClick={handleResume}>RESUME</button>
+      </div>
       </header>
     </div>
   );
